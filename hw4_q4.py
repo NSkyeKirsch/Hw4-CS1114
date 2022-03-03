73a8c717a6729fe35c131980d3135551e4849fc9
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-03-03 16:59:23 -0500

    Edited minor things

--- hw4_q4.py
+++ hw4_q4.py
@@ -1,6 +1,6 @@
 """
 Author: Noa Kirschbaum
-Assignment / Part: HW4 - Q2
+Assignment / Part: HW4 - Q4
 Date due: 2022-03-03
 I pledge that I have completed this assignment without
 collaborating with anyone else, in conformance with the
---
+++ hw4_q5.py
@@ -0,0 +1,9 @@
+"""
+Author: Noa Kirschbaum
+Assignment / Part: HW4 - Q4
+Date due: 2022-03-03
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+NYU School of Engineering Policies and Procedures on
+Academic Misconduct.
+"""
