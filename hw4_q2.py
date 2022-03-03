77018948689d72981edb5cfe2e43d11f6d4fbd30
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-03-03 18:39:31 -0500

    Finished q5

--- hw4_q2.py
+++ hw4_q2.py
@@ -11,25 +11,25 @@ Academic Misconduct.
 pos_n = 2*(int(input("Please enter a positive number: ")));
 loops_n = pos_n;
 num_half = round(pos_n/2);
-halfway = False;
+halfway_point = False;
 stars_str = "";
 num_rows = 0;
-spaces = 0;
+num_spaces = 0;
 
 while(num_rows < (pos_n - 1)):
     num_rows += 1;
     #print("num_rows:", num_rows);
     if (num_rows == num_half):
-        halfway = True;
-    for i in range(spaces):
+        halfway_point = True;
+    for i in range(num_spaces):
         stars_str += " ";
     for i in range(1,loops_n):
         stars_str += "*";
     print(stars_str);
-    if (loops_n > 1 and halfway == False):
+    if (loops_n > 1 and halfway_point == False):
         loops_n -= 2;
-        spaces += 1;
+        num_spaces += 1;
     else:
         loops_n += 2;
-        spaces -= 1;
+        num_spaces -= 1;
     stars_str = "";
--- hw4_q5.py
+++ hw4_q5.py
@@ -7,3 +7,18 @@ collaborating with anyone else, in conformance with the
 NYU School of Engineering Policies and Procedures on
 Academic Misconduct.
 """
+
+import turtle;
+
+polygon_n = int(input("Please enter the shape (as an integer): "));
+
+turtle.speed(0.2);
+
+for loop in range(20):
+    for i in range(polygon_n):
+        turtle.forward(100);
+        turtle.left(360/polygon_n);
+
+    turtle.left(360/20);
+
+
